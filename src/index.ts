924874cf0138b9ba07b598987bca8a6990c05ceb
Eryk Bielawski <erybie222@gmail.com>
2025-02-19 17:35:32 +0100

    footer and header goofy ass

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,12 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "..", "public", "views"));
+
+
 
 app.get("/", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "public", "index.ejs"));
+    res.render("index");
 })
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
