7059f8e04e2412188e29a6d26bf39f57fe1c04fa
Eryk Bielawski <erybie222@gmail.com>
2025-02-19 21:55:51 +0100

    navbar links and files restructization

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,22 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "..", "public", "views"));
+app.set("views", path.join(__dirname, "..", "views"));
+
 
 
 
 app.get("/", (req: Request, res: Response) => {
     res.render("index");
+})
+app.get("/recipes", (req: Request, res: Response) => {
+    res.render("pages/recipes");
+})
+app.get("/contact", (req: Request, res: Response) => {
+    res.render("pages/contact");
+})
+app.get("/about", (req: Request, res: Response) => {
+    res.render("pages/about");
 })
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
