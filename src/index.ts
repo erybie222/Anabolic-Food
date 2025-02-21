853738b4d3a0dcfcf5c743f09d69b82e7ad6546e
Eryk Bielawski <erybie222@gmail.com>
2025-02-21 12:49:26 +0100

    posting feature

--- src/index.ts
+++ src/index.ts
@@ -34,27 +34,53 @@ app.get("/recipes", async (req: Request, res: Response) => {
 })
 
  app.post("/add_recipe", async (req: Request, res: Response) => {
+    try {
+    await client.query("BEGIN");
+
     const newRecipe = {
         description: req.body.description,
         instruction: req.body.instruction,
         meal: req.body.meal,
         bulk_cut: req.body.bulk_cut,
     };
-    const [calories, proteins, fats, carbs]  = [req.body.calories , req.body.proteins, req.body.fats , req.body.carbs];
-    try {
-        
-    const result = await client.query(
+    const recipeResult = await client.query(
         "INSERT INTO RECIPES (description, instruction, meal, bulk_cut) VALUES ($1 , $2 , $3 , $4) RETURNING recipe_id", 
         Object.values(newRecipe)
     );
-        const recipeId = result.rows[0].recipe_id;
-        res.status(201).json({ message: "Recipe added", recipe_id: recipeId });
+    const recipeId = recipeResult.rows[0].recipe_id;
+ 
+
+    const calories  = [recipeId ,req.body.calories , req.body.proteins, req.body.fats , req.body.carbs];
+
+    
+    const caloriesResult = await client.query(
+        "INSERT INTO CALORIES (recipe_id , calories, proteins, fats, carbs ) VALUES ($1 , $2 , $3 , $4, $5) RETURNING recipe_id", 
+        Object.values(calories)
+    );
+          
+    
+    const photo = req.body.image;
+
+
+    const photoResult = await client.query(
+        "INSERT INTO PHOTOS (recipe_id , photo) VALUES ($1 , $2 ) RETURNING photo_id", 
+        [recipeId, photo]
+    );
+    await client.query("COMMIT");
+
+    res.status(201).json({
+        message: "Recipe, calories and image added successfully",
+        recipe_id: recipeId,
+        photo_id: photoResult.rows[0].photo_id
+        });
     }
     catch(err){
+        await client.query("ROLLBACK");
         console.log(err);
-        res.status(500).json({ message: "Error adding recipe" });
+        res.status(500).json({ message: "Error adding image" });
+        return;
     }
-
+    
 });
 
 app.get("/contact", (req: Request, res: Response) => {
