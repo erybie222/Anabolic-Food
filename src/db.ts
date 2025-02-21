3eedda2b0346783504389523bf77f6c9d802f439
Eryk Bielawski <erybie222@gmail.com>
2025-02-21 08:55:07 +0100

    DATABASE CONNECTION

---
+++ src/db.ts
@@ -0,0 +1,22 @@
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+});
+
+export async function connectDB() {
+    try {
+        await client.connect();
+        console.log('✅ Connection success');
+    } catch (err) {
+        console.error('❌ Failed to connect to database:', err);
+        process.exit(1);
+    }
+}
+
+export { client };
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from 'dotenv';
+import { connectDB , client } from "./db";
 
-
+dotenv.config();
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "..", "views"));
 
+connectDB();
+ async function getRecipes(){
+    const recipes = await client.query("SELECT * FROM RECIPES");
+    return recipes.rows;
+}
 
 
 
 app.get("/", (req: Request, res: Response) => {
     res.render("index");
 })
-app.get("/recipes", (req: Request, res: Response) => {
-    res.render("pages/recipes");
+app.get("/recipes", async (req: Request, res: Response) => {
+    const recipes = await getRecipes();
+    res.render("pages/recipes", {recipes: recipes});
+
 })
 app.get("/contact", (req: Request, res: Response) => {
     res.render("pages/contact");
@@ -26,3 +39,4 @@ app.get("/about", (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
+
