5b138da507a3e600994364624335dd3720ed01d6
Eryk Bielawski <erybie222@gmail.com>
2025-02-21 11:47:00 +0100

    post api 1

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "..", "views"));
+app.use(express.json());
+
 
 connectDB();
  async function getRecipes(){
@@ -31,9 +33,29 @@ app.get("/recipes", async (req: Request, res: Response) => {
 
 })
 
-// app.post("/recipes",async (req: Request, res: Response) => {
-    
-// });
+ app.post("/add_recipe", async (req: Request, res: Response) => {
+    const newRecipe = {
+        description: req.body.description,
+        instruction: req.body.instruction,
+        meal: req.body.meal,
+        bulk_cut: req.body.bulk_cut,
+    };
+    const [calories, proteins, fats, carbs]  = [req.body.calories , req.body.proteins, req.body.fats , req.body.carbs];
+    try {
+        
+    const result = await client.query(
+        "INSERT INTO RECIPES (description, instruction, meal, bulk_cut) VALUES ($1 , $2 , $3 , $4) RETURNING recipe_id", 
+        Object.values(newRecipe)
+    );
+        const recipeId = result.rows[0].recipe_id;
+        res.status(201).json({ message: "Recipe added", recipe_id: recipeId });
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ message: "Error adding recipe" });
+    }
+
+});
 
 app.get("/contact", (req: Request, res: Response) => {
     res.render("pages/contact");
